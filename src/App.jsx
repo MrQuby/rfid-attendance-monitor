ddae4265be8cf7c3917a4d40c4333cef1a3f92d3
MrQuby <alvinlag94@gmail.com>
2025-04-16 00:22:02 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import RfidAttendanceMonitor from './pages/RfidAttendanceMonitor'
+
+function App() {
+  return (
+    <RfidAttendanceMonitor />
+  )
+}
+
+export default App
